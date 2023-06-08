57b8379004ea21aaff6bc89e5c754bab2253568c
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-08 12:18:27 +0700

    1

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require("express");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
 const app = express();
 const upload = multer({
-  dest: "uploads/", // Direktori tujuan untuk menyimpan file
-});
-const { FileStorage } = require("@railway/app-tools");
-
-app.post("/upload", upload.single("file"), async (req, res) => {
-  try {
-    const file = req.file;
-    const fileId = await FileStorage.upload(file.originalname, file.path);
-    res.send(`File uploaded successfully. File ID: ${fileId}`);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error uploading file.");
-  }
+  dest: 'uploads/' // Direktori tujuan untuk menyimpan file
 });
 
-const port = process.env.PORT || 3000;
+app.post('/upload', upload.single('file'), (req, res) => {
+    try {
+      const file = req.file;
+      // Lakukan operasi yang diperlukan pada file
+      res.send('File uploaded successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error uploading file.');
+    }
+  });
+
+  const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`Server started on port ${port}`);
 });
